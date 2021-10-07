1950097d72a9c5694dbec02c6a74034fd0db0c0d
Hagop100 <htanashian@gmail.com>
2021-10-06 18:57:25 -0700

    Animation fix
    Player movement still needs work.
    Bug where player will not jump full height every now and then.
    wall jump needs to be disabled

--- Juicy Jumper/Assets/Scripts/PlayerController.cs
+++ Juicy Jumper/Assets/Scripts/PlayerController.cs
@@ -6,56 +6,119 @@ public class PlayerController : MonoBehaviour
 {
     //Inspector Variables
     [SerializeField] private float moveSpeed = 1f;
+    [SerializeField] private float jumpHeight = 1f;
+    [SerializeField] private float speedLimit = 1f;
     [SerializeField] private ParticleSystem dustParticle;
 
     //cache
     private Rigidbody2D myRigidBody;
     private Animator animator;
+    private BoxCollider2D myBoxCollider;
 
     //constant String references
     private const string IS_RUNNING = "isRunning"; //Animator bool for running animation
+    private const string IS_JUMPING = "isJumping"; //Animator trigger for jumping animation
+    private const string IS_LANDING = "isLanding"; //Animator trigger for landing animation
+    private const string FOREGROUND = "Foreground"; //LayerMask.GetMask()
+
+    //script variables
+    private bool isJumping = false;
+    private float faceRight = 1f;
+    private float moveHorizontal;
+    private float jumpVertical;
 
     // Start is called before the first frame update
     void Start()
     {
         myRigidBody = this.GetComponent<Rigidbody2D>();
         animator = this.GetComponent<Animator>();
+        myBoxCollider = this.GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        moveHorizontal = Input.GetAxisRaw("Horizontal");
+        jumpVertical = Input.GetAxisRaw("Vertical");
+
+        RunningAnimation(moveHorizontal);
+        JumpAnimation(jumpVertical); //Handles Jump and Land Animations
+        LimitSpeed(speedLimit);
     }
 
     private void FixedUpdate()
     {
-        PlayerMove();
+        PlayerMoveHorizontal(moveHorizontal);
+        Jump(jumpVertical);
     }
 
-    private void PlayerMove()
+    private void RunningAnimation(float input)
     {
-        float input = Input.GetAxisRaw("Horizontal");
-        if(input > 0)
+        if(input != 0)
         {
             animator.SetBool(IS_RUNNING, true);
-            myRigidBody.velocity = new Vector2(moveSpeed, myRigidBody.velocity.y);
-        }
-        else if(input < 0)
-        {
-            animator.SetBool(IS_RUNNING, true);
-            myRigidBody.velocity = new Vector2(-moveSpeed, myRigidBody.velocity.y);
         }
         else
         {
             animator.SetBool(IS_RUNNING, false);
         }
         FlipPlayer(input);
+
+        if(myRigidBody.velocity.y != 0)
+        {
+            animator.SetBool(IS_RUNNING, false);
+        }
+    }
+
+    private void JumpAnimation(float input)
+    {
+        if(input > 0)
+        {
+            animator.SetBool(IS_RUNNING, false);
+            animator.SetTrigger(IS_JUMPING);
+            isJumping = true;
+        }
+        else if (myRigidBody.velocity.y == 0 && isJumping == true)
+        {
+            LandAnimation();
+        }
+    }
+
+    private void LandAnimation()
+    {
+        animator.SetTrigger(IS_LANDING);
+        animator.ResetTrigger(IS_JUMPING);
+        isJumping = false;
+    }
+
+    private void PlayerMoveHorizontal(float input)
+    {
+        if (input > 0)
+        {
+            myRigidBody.AddForce(new Vector2(moveSpeed, 0f), ForceMode2D.Force);
+            //LimitSpeed(speedLimit);
+        }
+        else if (input < 0)
+        {
+            myRigidBody.AddForce(new Vector2(-moveSpeed, 0f), ForceMode2D.Force);
+            //LimitSpeed(speedLimit);
+        }
+    }
+
+    private void LimitSpeed(float speedLimit)
+    {
+        if(myRigidBody.velocity.x > speedLimit)
+        {
+            myRigidBody.velocity = new Vector2(speedLimit, myRigidBody.velocity.y);
+        }
+        else if(myRigidBody.velocity.x < -speedLimit)
+        {
+            myRigidBody.velocity = new Vector2(-speedLimit, myRigidBody.velocity.y);
+        }
     }
 
     private void FlipPlayer(float input)
     {
-        float faceRight = 1f;
         if(input > 0)
         {
             this.transform.localScale = new Vector2(faceRight, 1f);
@@ -69,6 +132,14 @@ public class PlayerController : MonoBehaviour
         else
         {
             this.transform.localScale = new Vector2(faceRight, 1f);
+        }
+    }
+
+    private void Jump(float input)
+    {
+        if(input > 0 && myBoxCollider.IsTouchingLayers(LayerMask.GetMask(FOREGROUND)))
+        {
+            myRigidBody.AddForce(new Vector2(0f, jumpHeight), ForceMode2D.Impulse);
         }
     }
 }
